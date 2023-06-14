c8cf060337d1ed8a1c8ee432d2524114e3e0911d
Martin Figueroa <supermacaco86@gmail.com>
2023-06-14 14:10:49 -0300

    primer form

--- src/components/todoApp.js
+++ src/components/todoApp.js
@@ -1,8 +1,41 @@
+import {useState} from "react"
+
 export default function TodoApp(){
+    const [title, setTitle] = useState("Hola");
+    const [todo, setTodo] = useState([]); 
+
+    function handleChange(e){
+        const value = e.target.value;
+        setTitle(value)
+    }
+
+    function handleSubmit(e){
+        e.preventDefault();
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title: title,
+            complited: false
+        };
+        const temp = [...todo]
+        temp.unshift(newTodo)
+        setTodo(temp);
+    }
+
     return <div className="todocontainer">
-        <form>
-            <input/>
-            <input type="submit" value="Create todo"/>
+        <form 
+        className="todoCreateForm"
+        onSubmit={handleSubmit}>
+            <input 
+            className="todoInput"
+            onChange={handleChange}
+            value={title}/>
+            <input 
+            className="buttonCreate" 
+            onClick={handleSubmit}
+            type="submit" 
+            value="Create todo"/>
+
+            {title}
         </form>
     </div>;
 }
